ef9e702c70566b7a1d0f1e24296ca707b83bc113
Jon Mountjoy <tech@jonmountjoy.com>
2015-06-25 13:50:23 +0100

    First bash at adding improved UI.  Need help doing SASS

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static(__dirname + '/public'));
 
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+// instruct express to server up static assets
+app.use(express.static('public'));
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.render('index')
 });
 
 app.listen(app.get('port'), function() {
