8450db10906e44b04acdf8bea15a59d04b6bfb26
c-hamilton <chamil03@calpoly.edu>
2017-11-11 13:33:37 -0800

    database testing

--- database_manager.js
+++ database_manager.js
@@ -5,9 +5,14 @@ var url =  "mongodb://wall:wall@ds259305.mlab.com:59305/heroku_fthfkxg6";
 console.log(url)
 // Connect to the db
 MongoClient.connect(url, function (err, db) {
-    console.log(db);
-    console.log(err);
+    if(err) { return console.dir(err); }
+    // https://mongodb.github.io/node-mongodb-native/api-articles/nodekoarticle1.html
     console.log('CONNECTED');
-     //Write databse Insert/Update/Query code here..
+    db.createCollection('test', {strict:true}, function(err, collection) {});
+    var collection = db.collection('test');
+    var doc1 = {'hello':'doc1'};
+    var doc2 = {'hello':'doc2'};
+    var lotsOfDocs = [{'hello':'doc3'}, {'hello':'doc4'}];
+    collection.insert(lotsOfDocs, {w:1}, function(err, result) {});
                 
 });
