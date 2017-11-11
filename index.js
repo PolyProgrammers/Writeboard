6c364ed22fe90f8b36fd28ba8961d18cc7636c1d
JoeDurand <joe.durand3@gmail.com>
2017-11-11 11:50:48 -0800

    add socket server, remove template files

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -13,6 +15,7 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
-app.listen(app.get('port'), function() {
+io.on('connection', function(socket){
+http.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
