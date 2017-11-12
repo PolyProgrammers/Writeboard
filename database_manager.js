affc094249e822224cc35489255d30a49361ae75
c-hamilton <chamil03@calpoly.edu>
2017-11-11 23:39:07 -0800

    Cloudinary picture working.

--- database_manager.js
+++ database_manager.js
@@ -1,7 +1,8 @@
 var MongoClient = require('mongodb').MongoClient,
     test = require('assert');
 var url =  "mongodb://wall:wall@ds259305.mlab.com:59305/heroku_fthfkxg6";
-    
+var databaseMode = false;
+
 
 class DatabaseManager {
     
@@ -10,36 +11,46 @@ class DatabaseManager {
         var callback = (db) => {
             this.database = db;
         }   
-        
-        MongoClient.connect(url, function (err, db) {
-            if(err) { return console.dir(err); }
-            // https://mongodb.github.io/node-mongodb-native/api-articles/nodekoarticle1.html
-            callback(db);
-        });        
+        if (databaseMode) {
+            MongoClient.connect(url, function (err, db) {
+                if(err) { return console.dir(err); }
+                // https://mongodb.github.io/node-mongodb-native/api-articles/nodekoarticle1.html
+                callback(db);
+            });        
+        } else {
+           console.log("database mode is off")
+        }
     }
     
-    getAll(callback) {
-        var collection = this.database.collection('wall');
-        collection.find().toArray(function(err, items) {
-            callback(items);
-        });
+    getAll(callback) { 
+        if (databaseMode) {
+            var collection = this.database.collection('wall');
+            collection.find().toArray(function(err, items) {
+                callbackWith(items)
+            });
+        }
     }
 
     putDummyData() {
-        var collection = this.database.collection('test2');
         var doc1 = {'hello':'doc1'};
         var doc2 = {'hello':'doc2'};
         var lotsOfDocs = [{'hello':'doc3'}, {'hello':'doc4'}, doc1, doc2];
-        this.database.createCollection('test2', {strict:true}, function(err, collection) {});
-        collection.insert(lotsOfDocs, {w:1}, function(err, result) {});
-        var collection = this.database.collection('test2');
+        if (databaseMode) {
+           var collection = this.database.collection('test2');
+           this.database.createCollection('test2', {strict:true}, function(err, collection) {});
+           collection.insert(lotsOfDocs, {w:1}, function(err, result) {});
+           var collection = this.database.collection('test2');
+        }
     }
 
     update(key, record) {
-        var collection = this.database.collection('wall');
-        collection.update(key, {$set:record}, {upsert: true}, function(err, result) {
-            if(err) { return console.dir(err); }
-        });
+        if (databaseMode) {
+            var collection = this.database.collection('wall');
+            collection.update(key, {$set:record}, {upsert: true}, function(err, result) {
+               if(err) { return console.dir(err); }
+            });
+        }
+
     }
 }
 
--- public/javascript/board.js
+++ public/javascript/board.js
@@ -2,20 +2,23 @@ var container = $("#contentContainer");
 container.click(getClickPosition);
 
 function uuidv4() {
-return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-    return v.toString(16);
-});
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
 }
 
 var newText = function(params) {
     $.tmpl(input, params).appendTo( "#contentContainer" );
     var ele = $('#' + params.uuid);
-
     ele.val(params.text);
-
     return ele;
 }
+
+//function containerClicked(e) {
+//   var position = e.getClickPosition;
+//   // TODO gabby is you guuuuuuuurllllllll
+//}
 
 function getClickPosition(e) {
     var parentPosition = getPosition(e.currentTarget);
@@ -64,16 +67,16 @@ function getPosition(el) {
 
     while (el) {
         if (el.tagName == "BODY") {
-        // deal with browser quirks with body/window/document and page scroll
-        var xScroll = el.scrollLeft || document.documentElement.scrollLeft;
-        var yScroll = el.scrollTop || document.documentElement.scrollTop;
+            // deal with browser quirks with body/window/document and page scroll
+            var xScroll = el.scrollLeft || document.documentElement.scrollLeft;
+            var yScroll = el.scrollTop || document.documentElement.scrollTop;
 
-        xPos += (el.offsetLeft - xScroll + el.clientLeft);
-        yPos += (el.offsetTop - yScroll + el.clientTop);
+            xPos += (el.offsetLeft - xScroll + el.clientLeft);
+            yPos += (el.offsetTop - yScroll + el.clientTop);
         } else {
-        // for all other non-BODY elements
-        xPos += (el.offsetLeft - el.scrollLeft + el.clientLeft);
-        yPos += (el.offsetTop - el.scrollTop + el.clientTop);
+            // for all other non-BODY elements
+            xPos += (el.offsetLeft - el.scrollLeft + el.clientLeft);
+            yPos += (el.offsetTop - el.scrollTop + el.clientTop);
         }
 
         el = el.offsetParent;
@@ -83,3 +86,54 @@ function getPosition(el) {
         y: yPos
     };
 }
+
+container.dblclick(function(el) {
+    alert( "Handler for .dblclick() called." );
+    cloudinaryUpload(el);
+});
+
+function cloudinaryUpload(e) {
+    var uuid = uuidv4();
+    var name = "wallflower_" + uuid;
+    var options = {
+      public_id: name, 
+      cloud_name: 'writeboard',
+      upload_preset: 'wallphotos',
+      sources: ['local','url','camera']
+      //sources: ['local','url','camera','dropbox',   'image_search', 'facebook']
+    };
+    
+    var parentPosition = getPosition(e.currentTarget);
+    var xPosition = e.clientX - parentPosition.x;
+    var yPosition = e.clientY - parentPosition.y;
+    //generate the textfield
+    var params = {
+        "uuid": uuid,
+        "x" : xPosition,
+        "y" : yPosition
+    };
+    
+    cloudinary.openUploadWidget(options, 
+      function(error, result) {
+      
+      if (error) {
+        console.log(error);
+      }
+      if (result) {  
+        render(result[0].public_id, params);
+        params.photo = result[0].public_id;
+        console.log(result);
+      }
+    });
+  }
+  
+  var render = (id, params) => {
+    var url = "http://res.cloudinary.com/writeboard/image/upload/t_thumbnail-round/" + id;
+    console.log(url);
+    var img = $('<img>');
+    img.attr('id', id);
+    img.attr('src', url); //need to create the attrubyte
+    img.attr('style','left:' + params.x +'px;top:' + params.y +'px;' + 'position:absolute');
+    img.appendTo(container);
+    console.log("finished adding image");
+  };
